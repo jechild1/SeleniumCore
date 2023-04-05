53d25a3e94e878bb0f40bb98047dba11ecfc065b
jesse <jesse@Aretec-Laptop>
2023-04-05 08:37:39 -0400

    Second Commit

---
+++ SeleniumCore/src/CoreConfig/CoreConfig.java
@@ -0,0 +1,26 @@
+package CoreConfig;
+
+/**
+ * Class containing methods to set up browser type and configuration.
+ * 
+ * @author jesse
+ *
+ */
+public abstract class CoreConfig {
+	
+	protected static WebDriver driver;
+	
+	
+
+	/**
+	 * Abstract config constructor
+	 */
+	public CoreConfig() {
+		// Nothing to see here
+	}
+	
+	
+	
+	
+
+}
